87d2fe3cc9c97c258730008c982e42e14a173d1b
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-21 21:34:49 +0100

    changed the import for the svgs, got them from undraw but not txt not moving to the right

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -4,12 +4,13 @@ export const homeObjOne = {
   lightText: true,
   lightTextDesc: true,
   topLine: "Hacker's Hub",
-  headline: 'Learn from real cases and real hackers from around the world.',
+  headline:
+    'Learn from real world cases and amazing community from around the world.',
   description:
     'Get access to more real life virtual machines with our pro version!',
   buttonLabel: 'Get started',
   imgStart: false,
-  img: require('../../images/svg-1.svg'),
+  img: require('../../images/svg-2.svg').default,
   alt: 'Hacker',
   dark: true,
   primary: true,

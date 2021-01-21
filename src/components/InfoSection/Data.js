c765b84df9db077b7c03e376b229ae9c2ddbd7f3
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-21 20:50:55 +0100

    created data file of easy styling in the future.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Hacker's Hub",
+  headline: 'Learn from real cases and real hackers from around the world.',
+  description:
+    'Get access to more real life virtual machines with our pro version!',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg'),
+  alt: 'Hacker',
+  dark: true,
+  primary: true,
+  darkText: false,
+};

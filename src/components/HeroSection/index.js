0e44476153027689a11217f55c061a845a8b72ab
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-21 19:51:25 +0100

    started to style the elements of Info

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -24,8 +24,8 @@ const HeroSection = () => {
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
-        <HeroH1>Visit New York City!</HeroH1>
-        <HeroP>Sign up for amazing discounts</HeroP>
+        <HeroH1>Learn to Become an Ethical Hacker!</HeroH1>
+        <HeroP>Sign up samurai, we have city to hack.</HeroP>
         <HeroBtnWrapper>
           <Button
             to="signup"

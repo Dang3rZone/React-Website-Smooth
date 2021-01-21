3d0ed0faf6069a8100caeca3bae1d2ce18953608
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-21 22:04:10 +0100

    implemente same styles for the other  reusable sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -9,8 +9,46 @@ export const homeObjOne = {
   description:
     'Get access to more real life virtual machines with our pro version!',
   buttonLabel: 'Get started',
-  imgStart: false,
-  img: require('../../images/svg-9.svg').default,
+  imgStart: true,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Hacker',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Hacker's Hub",
+  headline:
+    'Learn from real world cases and amazing community from around the world.',
+  description:
+    'Get access to more real life virtual machines with our pro version!',
+  buttonLabel: 'Get started',
+  imgStart: true,
+  img: require('../../images/svg-2.svg').default,
+  alt: 'Hacker',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjThree = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Hacker's Hub",
+  headline:
+    'Learn from real world cases and amazing community from around the world.',
+  description:
+    'Get access to more real life virtual machines with our pro version!',
+  buttonLabel: 'Get started',
+  imgStart: true,
+  img: require('../../images/svg-3.svg').default,
   alt: 'Hacker',
   dark: true,
   primary: true,

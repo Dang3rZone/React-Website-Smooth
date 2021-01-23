12d2ea0b95b1b2f9b0ad2b9dd59c41bfc051197a
Andres Rosales <andresrosalespinzon@gmail.com>
2021-01-23 13:28:52 +0100

    started creating the footer elements

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -36,13 +36,13 @@ export const ServicesCard = styled.div`
   background: #fff;
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
   border-radius: 10px;
-  max-height: 340px;
+  max-height: 390px;
   padding: 30px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
   transition: all 0.2s ease-in-out;
-  justify-content: flex-start;
   &:hover {
     transform: scale(1.02);
     transition: all 0.2s ease-in-out;
